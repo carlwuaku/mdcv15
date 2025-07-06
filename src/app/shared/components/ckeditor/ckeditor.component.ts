3c794fd6014422d9250bb13f0af164638c4c9192
wuakuc <wuakuc@gmail.com>
2025-07-06 11:20:57 +0100

    fix html in ckeditor

--- src/app/shared/components/ckeditor/ckeditor.component.ts
+++ src/app/shared/components/ckeditor/ckeditor.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import Editor from 'ckeditor5-custom-build/build/ckeditor';
+import { EditorConfig } from '@ckeditor/ckeditor5-core';
 
 @Component({
   selector: 'app-ckeditor',
@@ -8,17 +9,32 @@ import Editor from 'ckeditor5-custom-build/build/ckeditor';
 })
 export class CkeditorComponent {
   public Editor = Editor.Editor;
-  @Input() name:string = "";
+  @Input() name: string = "";
   @Input() value: string = '';
-  @Output() valueChange:EventEmitter<string> = new EventEmitter<string>();
+  @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
+
+  // Configuration to preserve HTML structure and styling
+  public config: EditorConfig = {
+    htmlSupport: {
+      allow: [
+        {
+          name: /.*/, // Allow all elements
+          attributes: true, // Allow all attributes
+          classes: true,  // Allow all classes
+          styles: true   // Allow all styles
+        }
+      ],
+      allowEmpty: ['div', 'p', 'i', 'span', 'img']
+    }
+  };
 
   onReady(editor: any) {
-    editor.editing.view.change( (writer:any) => {
-      writer.setStyle( 'height', '200px', editor.editing.view.document.getRoot() );
-  } );
+    editor.editing.view.change((writer: any) => {
+      writer.setStyle('height', '300px', editor.editing.view.document.getRoot());
+    });
   }
 
-  valueChanged(){
+  valueChanged() {
     this.valueChange.emit(this.value);
   }
 }
