2038de1b517bc171bfb7d3638274d6d0cbe0e8a4
Carl Wuaku <39660120+carlwuaku@users.noreply.github.com>
2025-07-17 21:08:56 +0100

    Feature/add firebase workflows (#45)
    * add new deployment actions for firebase
    
    * add ftp deployment for pcms uat
    
    * add workflows for the different sites and stages

---
+++ src/environments/environment.mdcms-production.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  name: "prod",
+
+  host: "https://licensing.druglanepms.com/",
+  routeTracing: false,
+  googleAnalyticsTrackingId: ""
+};
---
+++ src/environments/environment.pcms-production.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  name: "prod",
+
+  host: "https://management.pcghana.org/",
+  routeTracing: false,
+  googleAnalyticsTrackingId: ""
+};
