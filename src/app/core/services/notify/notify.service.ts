479112047be06498068fd0fbeae39c1e7d7961f5
Carl Wuaku <39660120+carlwuaku@users.noreply.github.com>
2025-08-14 05:28:16 +0100

    fix forgot password, add dialog to notify, add template designer to edit settings, update ckeditor in edit settings, use (#69)

--- src/app/core/services/notify/notify.service.ts
+++ src/app/core/services/notify/notify.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from 'src/app/shared/components/dialog/dialog.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotifyService {
 
-  constructor(private messageService: MessageService, private snackBar: MatSnackBar) { }
+  constructor(private messageService: MessageService, private dialog: MatDialog) { }
 
   showNotification(message: string, type: "success" | "info" | "warn" | "error", delay?: number) {
-    
+
     this.messageService.add({
       severity: type, summary: type,
       detail: message
@@ -21,11 +23,11 @@ export class NotifyService {
     this.showNotification("Unable to send data to server. Please try again or contact admin", "error", 3000);
   }
 
-  successNotification(text:string) {
+  successNotification(text: string) {
     this.showNotification(text, "success", 3000);
   }
 
-  failNotification(text:string) {
+  failNotification(text: string) {
     this.showNotification(text, "error", 3000);
   }
 
@@ -34,7 +36,8 @@ export class NotifyService {
   }
 
   showLoading() {
-    this.messageService.add({key: 'loading',
+    this.messageService.add({
+      key: 'loading',
       severity: 'info', summary: 'Loading'
     });
   }
@@ -43,8 +46,18 @@ export class NotifyService {
     this.messageService.clear('loading');
   }
 
-  infoNotification(text:string) {
+  infoNotification(text: string) {
     this.showNotification(text, "info", 3000);
   }
-  
+
+  warningAlertNotification(title: string, text: string) {
+    this.dialog.open(DialogComponent, {
+      data: {
+        title,
+        message: text,
+        icon: "warning"
+      }
+    })
+  }
+
 }
